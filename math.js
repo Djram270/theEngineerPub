da8f30240c663c8393cd775a8fb510d478d339e0
Oscar Tosne <oskr98tossn@gmail.com>
2022-06-23 11:32:23 -0500

    Changed name of radiousEarth to vectors

---
+++ math.js
@@ -0,0 +1,8 @@
+var radioPlaneta = 0;
+var gravedadPlaneta = 0;
+var duracionDiapLaneta = 0;
+var velocidadAngular = 0;
+
+velocidadAngular = ((2*Math.PI)/duracionDiapLaneta);
+radioPlaneta = gravedadPlaneta/((velocidadAngular)^2);
+
