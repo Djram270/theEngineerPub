411857e0319a570a4751976bc41636a567a85d44
Oscar Tosne <oskr98tossn@gmail.com>
2022-06-28 10:22:18 -0500

    Direction operation added. Final commit for the presentation

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const navLinks = document.getElementsByClassName('nav__links')[0]
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
 })
+
+const navAnimations = () =>{
+    const nav = document.querySelector('.navbar');
+
+        window.addEventListener('scroll', () =>{
+            nav.style.background = ('white');
+        });
+}
--- vectors.js
+++ vectors.js
@@ -1,8 +1,10 @@
 var buttonMagnitude = document.getElementById("magnitudebutton");
+var buttonDirection = document.getElementById("directionbutton");
 var inputVector1 = document.getElementById("text_vector1");
 var inputVector2 = document.getElementById("text_vector2");
 
 buttonMagnitude.addEventListener("click", vectorCalculateMagnitude);
+buttonDirection.addEventListener("click", vectorCalculateDirection);
 
 function separateComponents(prmComponets){ //This function converts the 'inputVector1' array(String) into an array(Int) called 'arrayIntV1'
     varComponets = prmComponets.value;
@@ -10,8 +12,6 @@ function separateComponents(prmComponets){ //This function converts the 'inputVe
     var arrayIntV1 = varComponets.split(',').map(function(item){
         return parseInt(item,10);
     });
-
-    console.log(arrayIntV1);
     return arrayIntV1;
 }
 
@@ -23,7 +23,6 @@ class Vector {
         var magni = 0;
         for(var i=0; i<this.entradas.length;i++){
             magni = Math.pow(this.entradas[i],2) + magni;
-            console.log(magni);
         }
         magni = Math.sqrt(magni);
         return magni;
@@ -32,11 +31,27 @@ class Vector {
     get magnitude(){
         return this.calculateMagnitude();
     }
+
+    calculateDirection(){
+        var dire = 0;
+        dire = Math.atan(this.entradas[1]/this.entradas[0])*(180/Math.PI);
+        return dire;
+    }
+
+    get direction(){
+        return this.calculateDirection();
+    }
 }
 
 function vectorCalculateMagnitude(){
     var vector1 = new Vector(separateComponents(inputVector1));
     document.querySelector('#imprimir').innerHTML = vector1.magnitude;
+}
+
+function vectorCalculateDirection(){
+    var vector2 = new Vector(separateComponents(inputVector2));
+    document.querySelector('#imprimir2').innerHTML = vector2.direction;
+
 }
 
 
